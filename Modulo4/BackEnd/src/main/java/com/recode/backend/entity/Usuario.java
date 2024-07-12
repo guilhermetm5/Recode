9949aaae2194d40166acc8a5a275831f434f2758
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-12 14:46:21 -0300

    adicionando getters and setters

--- Modulo4/BackEnd/src/main/java/com/recode/backend/entity/Usuario.java
+++ Modulo4/BackEnd/src/main/java/com/recode/backend/entity/Usuario.java
@@ -28,6 +28,55 @@ public class Usuario {
 		this.telefone = telefone;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
 	
 }
 

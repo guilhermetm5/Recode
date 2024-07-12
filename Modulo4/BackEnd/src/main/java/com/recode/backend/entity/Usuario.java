42790543addcf7b4d1516f183ff6288fc7fca3d9
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-12 14:45:28 -0300

    adicionando construtores

--- Modulo4/BackEnd/src/main/java/com/recode/backend/entity/Usuario.java
+++ Modulo4/BackEnd/src/main/java/com/recode/backend/entity/Usuario.java
@@ -1,5 +1,10 @@
 package com.recode.backend.entity;
 
+/*
+ *	Autor: João Guilherme em 12/07/2024,
+ * 	Para entrega e avaliação da Recode ProAldeia. 
+ */
+
 public class Usuario {
 	
 	private int id;
@@ -8,7 +13,22 @@ public class Usuario {
 	private String dataNasc;
 	private String email;
 	private String telefone;
+	
+	public Usuario() {
+		super();
+	}
 
+	public Usuario(int id, String nome, int idade, String dataNasc, String email, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+		this.dataNasc = dataNasc;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	
 }
 
 

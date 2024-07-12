e0d2fd246f0a11e548925b7790cb8f9a92fd1f01
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-12 14:35:23 -0300

    adicionando a entidade Usuario

---
+++ Modulo4/BackEnd/src/main/java/com/recode/backend/entity/Usuario.java
@@ -0,0 +1,14 @@
+package com.recode.backend.entity;
+
+public class Usuario {
+	
+	private int id;
+	private String nome;
+	private int idade;
+	private String dataNasc;
+	private String email;
+	private String telefone;
+
+}
+
+
